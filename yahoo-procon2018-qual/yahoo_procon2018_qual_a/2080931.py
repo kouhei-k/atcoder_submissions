f757c2e46fec2b2ebde1d321856824dc2c25dbea
kouhei-k <kouhei.k.0116@gmail.com>
2018-10-02 22:11:37 +0900

    add yahoo-procon2018-qual/yahoo_procon2018_qual_a/2080931.py

---
+++ yahoo-procon2018-qual/yahoo_procon2018_qual_a/2080931.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+input_line = raw_input()
+
+if input_line[0] == 'y':
+    if input_line[1] == 'a':
+        if input_line[2] == 'h':
+            if input_line[3] == input_line[4]:
+                print "YES"
+
+            else:
+                print "NO"
+        else:
+            print "NO"
+    else:
+        print "NO"
+else:
+    print "NO"
