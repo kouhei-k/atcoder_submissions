3b2359ac9652937c9a618f72730119f26ba000b4
kouhei-k <kouhei.k.0116@gmail.com>
2019-01-08 22:12:19 +0900

    add abc088/abc088_c/7051111.py

---
+++ abc088/abc088_c/7051111.py
@@ -0,0 +1,27 @@
+c = [[]]*3
+
+for i in range(3):
+    c[i] = list(map(int, input().split()))
+
+
+arr = [0]*3
+sum = 0
+for i in range(3):
+    arr[0] += c[i][i]
+for i in range(3):
+    if i + 1 < 3:
+        arr[1] += c[i][i+1]
+    else:
+        arr[1] += c[i][0]
+for i in range(3):
+    if i + 2 < 3:
+        arr[2] += c[i][i+2]
+    else:
+        arr[2] += c[i][i-1]
+
+if arr[0] != arr[1] or arr[0] != arr[2]:
+    print("No")
+    exit(0)
+
+print("Yes")
+exit(0)
