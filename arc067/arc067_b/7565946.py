f9dbd0d752debdb280ef323deaf3c69553ef1989
kouhei-k <kouhei.k.0116@gmail.com>
2019-03-09 22:15:05 +0900

    add arc067/arc067_b/7565946.py

---
+++ arc067/arc067_b/7565946.py
@@ -0,0 +1,11 @@
+N, A, B = map(int, input().split())
+
+X = list(map(int, input().split()))
+ans = 0
+for i in range(N-1):
+    dis = X[i+1] - X[i]
+    if dis * A > B:
+        ans += B
+    else:
+        ans += dis * A
+print(ans)
