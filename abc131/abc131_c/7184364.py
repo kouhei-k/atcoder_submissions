daefd070b3f83bd98920e445dffba285d9d40e2a
kouhei-k <kouhei.k.0116@gmail.com>
2019-05-08 22:12:35 +0900

    add abc131/abc131_c/7184364.py

---
+++ abc131/abc131_c/7184364.py
@@ -0,0 +1,8 @@
+import fractions
+A,B,C,D = map(int,input().split())
+
+lcm = int(C*D / fractions.gcd(C,D))
+ans_b = B - (B // C) - (B // D) + (B // lcm)
+ans_a = A - 1 - ((A - 1)// C) - ((A - 1)// D) + ((A - 1) // lcm)
+
+print(ans_b - ans_a)
