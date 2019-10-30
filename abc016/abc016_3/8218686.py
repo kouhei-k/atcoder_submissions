93d39b54a373ae2b3f9ee5f1bc1c928990879ab4
kouhei-k <kouhei.k.0116@gmail.com>
2019-10-30 22:22:35 +0900

    add abc016/abc016_3/8218686.py

---
+++ abc016/abc016_3/8218686.py
@@ -0,0 +1,21 @@
+import collections
+N, M = map(int, input().split())
+A = [0]*M
+B = [0]*M
+for i in range(M):
+    A[i], B[i] = map(int, input().split())
+
+table = collections.defaultdict(set)
+for i in range(M):
+    table[A[i]].add(B[i])
+    table[B[i]].add(A[i])
+
+for i in range(N):
+    tmp = set()
+    for x in table:
+        if x == i+1:
+            continue
+        if i+1 in table[x]:
+            tmp |= table[x] - {i+1}
+    tmp -= table[i+1]
+    print(len(tmp))
