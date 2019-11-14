bb18b11f7ff125e12c41d058ab06c72a6c61f592
kouhei-k <kouhei.k.0116@gmail.com>
2019-11-14 20:51:14 +0900

    add nikkei2019-ex/nikkei2019ex_f/8432665.py

---
+++ nikkei2019-ex/nikkei2019ex_f/8432665.py
@@ -0,0 +1,17 @@
+N=int(input())
+for i in range(1,N+1):
+  ans=[]
+  if i % 2==0:
+    ans.append("a")
+  if i %3==0:
+    ans.append("b")
+  if i %4==0:
+    ans.append("c")
+  if i %5==0:
+    ans.append("d")
+  if i%6==0:
+    ans.append("e")
+  if len(ans)==0:
+    print(i)
+  else:
+    print("".join(ans))
