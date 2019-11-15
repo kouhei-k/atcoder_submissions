260ace1117e053921ad6facbd8ffd32b78858f17
kouhei-k <kouhei.k.0116@gmail.com>
2019-11-15 20:59:45 +0900

    add abc135/abc135_c/8444587.py

---
+++ abc135/abc135_c/8444587.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans = 0
+for i in range(N):
+    ans += min(A[i], B[i])
+    ans += min(A[i+1], B[i] - min(A[i], B[i]))
+    A[i+1] -= min(B[i] - min(A[i], B[i]), A[i+1])
+
+print(ans)
