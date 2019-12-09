a2dff780a397db21e8087eb6699c9082016062ee
kouhei-k <kouhei.k.0116@gmail.com>
2019-12-09 22:13:23 +0900

    add arc100/arc100_a/7475252.py

---
+++ arc100/arc100_a/7475252.py
@@ -0,0 +1,25 @@
+import math
+N=int(input())
+A=list(map(int,input().split()))
+
+B=[A[i]-(i+1) for i in range(N)]
+B.sort()
+temp=0
+
+if N%2==0:
+  ans = B[N//2]
+  ans2= B[N//2 -1]
+  temp=0
+  for j in range(N):
+    temp+= abs(B[j] - ans)
+  temp2=0
+  for j in range(N):
+    temp2+= abs(B[j] - ans2)
+  ans= min(temp,temp2)
+else:
+  ans = B[N//2]
+  temp=0
+  for j in range(N):
+    temp+= abs(B[j] - ans )
+  ans=temp
+print(ans)
