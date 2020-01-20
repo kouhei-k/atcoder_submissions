19cb0c388035df117f9123f81c6edf147134ef60
kouhei-k <kouhei.k.0116@gmail.com>
2020-01-20 18:20:44 +0900

    add abc096/abc096_d/9644679.py

---
+++ abc096/abc096_d/9644679.py
@@ -0,0 +1,30 @@
+def primes(n):
+    ass = []
+    is_prime = [True] * (n + 1)
+    is_prime[0] = False
+    is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if not is_prime[i]:
+            continue
+        for j in range(i * 2, n + 1, i):
+            is_prime[j] = False
+    for i in range(len(is_prime)):
+        if is_prime[i]:
+            ass.append(i)
+    return ass
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+from itertools import combinations
+N=int(input())
+
+prime_list=primes(55555)
+id=4
+ans=[prime_list[i] for i in range(len(prime_list)) if prime_list[i]%5==2]
+
+print(" ".join(map(str,ans[:N])))
