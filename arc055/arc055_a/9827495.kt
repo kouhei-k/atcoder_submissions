293bc982e08e4123204ea63b5b168965539782b8
kouhei-k <kouhei.k.0116@gmail.com>
2020-01-30 21:00:09 +0900

    add arc055/arc055_a/9827495.kt

---
+++ arc055/arc055_a/9827495.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    var ans = "1"
+    for (no in 1..n-1){
+        ans += "0"
+    }
+    
+    ans += "7"
+    println(ans)
+    
+}
