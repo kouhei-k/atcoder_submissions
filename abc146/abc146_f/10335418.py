4db95a8259fb8fbb11165fb05ee8e919e7c54f1d
kouhei-k <kouhei.k.0116@gmail.com>
2020-02-25 08:00:12 +0900

    add abc146/abc146_f/10335418.py

---
+++ abc146/abc146_f/10335418.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+S = input()
+
+p = N
+prev = N
+flag = True
+ans = []
+for i in range(N, -1, -1):
+    if (prev - i) > M:
+        ans.append(prev - p)
+        prev = p
+        if (prev - i) > M:
+            flag = False
+            break
+    if S[i] == "0":
+        p = i
+ans.append(prev)
+if flag:
+    print(" ".join(map(str, ans[::-1])))
+else:
+    print(-1)
