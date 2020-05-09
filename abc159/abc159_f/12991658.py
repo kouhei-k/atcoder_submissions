791f25f117e34d724b25d3fe871a01d7f275e99f
kouhei-k <kouhei.k.0116@gmail.com>
2020-05-09 23:00:55 +0900

    add abc159/abc159_f/12991658.py

---
+++ abc159/abc159_f/12991658.py
@@ -0,0 +1,20 @@
+def main():
+    N, S = map(int, input().split())
+    A = list(map(int, input().split()))
+    mod = 998244353
+    ans = 0
+    dp = [0]*S
+
+    for i, a in enumerate(A):
+        dp[0] +=1
+        if a > S:
+            continue
+
+        ans += dp[S-a]*(N-i)
+
+        for j, x in enumerate(dp[:S-a]):
+            dp[j+a] += x
+    print(ans % mod)
+
+
+main()
