6c4b33a1d69b6873d3790f1a497b0ec710abc4b7
kouhei-k <kouhei.k.0116@gmail.com>
2020-05-13 21:06:53 +0900

    add abc096/abc096_c/13178470.py

---
+++ abc096/abc096_c/13178470.py
@@ -0,0 +1,17 @@
+H, W = map(int, input().split())
+s = [input() for i in range(H)]
+for i in range(H):
+    for j in range(W):
+        if s[i][j] == '#':
+            if i > 0 and s[i-1][j] == '#':
+                continue
+            elif i < H-1 and s[i+1][j] == '#':
+                continue
+            elif j > 0 and s[i][j-1] == '#':
+                continue
+            elif j < W-1 and s[i][j+1] == '#':
+                continue
+            else:
+                print("No")
+                exit(0)
+print("Yes")
