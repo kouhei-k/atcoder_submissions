50488ceea427a5473d27f147474c5dc4d45c3afa
kouhei-k <kouhei.k.0116@gmail.com>
2020-10-30 20:13:57 +0900

    add joi2009yo/joi2009yo_b/17733059.py

---
+++ joi2009yo/joi2009yo_b/17733059.py
@@ -0,0 +1,9 @@
+W = []
+K = []
+for i in range(10):
+    W.append(int(input()))
+for i in range(10):
+    K.append(int(input()))
+W.sort(reverse=True)
+K.sort(reverse=True)
+print(sum(W[:3]), sum(K[:3]))
