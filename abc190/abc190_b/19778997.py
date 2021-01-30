32b5053a8f29aa1b56586646ee59c846eae696bc
kouhei-k <kouhei.k.0116@gmail.com>
2021-01-30 21:05:32 +0900

    add abc190/abc190_b/19778997.py

---
+++ abc190/abc190_b/19778997.py
@@ -0,0 +1,10 @@
+N, S, D = map(int, input().split())
+flag = False
+for i in range(N):
+    x, y = map(int, input().split())
+    if x < S and y > D:
+        flag = True
+if flag:
+    print("Yes")
+else:
+    print("No")
